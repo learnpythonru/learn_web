9b0ea1b8bb22bb40c894c27e62c59966d972fd78
Mikhael Korneev <korneevm@gmail.com>
2020-10-30 17:57:57 +0300

    Final version

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
 from webapp.db import db
 
 class News(db.Model):
@@ -7,5 +10,28 @@ class News(db.Model):
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
 
+    def comments_count(self):
+        return Comment.query.filter(Comment.news_id == self.id).count()
+
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+    news = relationship('News', backref='comments')
+    user = relationship('User', backref='comments')
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,13 +4,14 @@ from flask_login import current_user, login_user, logout_user
 from webapp.db import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
 
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
     title = "Авторизация"
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
@@ -24,7 +25,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
 
     flash('Неправильные имя пользователя или пароль')
     return redirect(url_for('user.login'))
